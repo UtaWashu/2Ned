1bebb88869654764d110a6bf9a200d2be3caf718
UtaWashu <86579882+UtaWashu@users.noreply.github.com>
2024-05-20 11:35:36 +0300

    Create 20.Valid Parentheses.cpp

---
+++ 20/20.Valid Parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+for (char c : s) {
+if (c == '(' || c == '{' || c == '[') {
+stack.push(c);
+} else {
+if (stack.empty()) {
+return false;
+}
+char open = stack.top();
+stack.pop();
+if ((c == ')' && open != '(') || (c == '}' && open != '{') || (c == ']' && open != '[')) {
+return false;
+}
+}
+}
+return stack.empty();
+    }
+};
